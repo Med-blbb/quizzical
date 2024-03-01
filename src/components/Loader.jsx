7512185b999b41ac01d07d17a1126f10befb001c
MliliGenes <saad.elm.77@gmail.com>
2024-03-01 02:06:28 +0000

    updates on the category cards style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import "./LoaderStyle.css"
+import React from "react";
+import "./LoaderStyle.css";
 
 export default function Loader() {
   return (
-    <div className="loadingspinner">
-      <div id="square1"></div>
-      <div id="square2"></div>
-      <div id="square3"></div>
-      <div id="square4"></div>
-      <div id="square5"></div>
+    <div className="wrapper">
+      <section class="dots-container">
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+      </section>
     </div>
   );
 }
